63440c81167c383118617a0e10276e3604e82d20
Mike Wadden <mike.wadden@keyin.com>
2022-10-31 16:51:11 -0230

    FrameWork

---
+++ Project/my-portfolio/src/Components/Contact/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactForm from './contactForm';
+
+const Contact = () => {
+    return (
+      <section>
+        <ContactForm/>  
+      </section>
+    );
+  };
+  
+  export default Contact;

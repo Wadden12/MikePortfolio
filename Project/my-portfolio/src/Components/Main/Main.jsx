32008772f91d7ba3a6a0bdcca6ff71a0b2ef13ca
Mike Wadden <mike.wadden@keyin.com>
2022-10-31 12:23:49 -0230

    Base compnet added

---
+++ Project/my-portfolio/src/Components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+const Main = () => {
+  return (
+    <Fragment>
+       <Header/> 
+    <div>This is Main</div>
+    <footer>
+    <Footer/>
+    </footer>
+    </Fragment>
+  )
+}
+
+export default Main
---
+++ Project/my-portfolio/src/Components/Main/Nav.jsx
@@ -0,0 +1,12 @@
+
+export default function NavBar(){
+
+    return(
+        <nav>
+            <div>
+                <h1>This is that Nav</h1>
+            </div>
+        </nav>
+    )
+
+}
